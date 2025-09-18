861dd9eda488bf7cf5fcc3ed427c340a197f4c88
stmball <stmball@gmail.com>
2025-09-18 10:08:50 +0100

    Dynamic root path

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// The base path is set to the environment variable, or '/' for local development
+const VITE_BASE_PATH = process.env.VITE_BASE_PATH || '/';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: VITE_BASE_PATH,
   plugins: [
     tailwindcss(),
     react()
