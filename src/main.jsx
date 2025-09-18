c9481513c7909a17884564e9524a938f378c5f0e
stmball <stmball@gmail.com>
2025-09-18 10:15:06 +0100

    React-router Fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
+const routerBaseName = import.meta.env.VITE_BASE_PATH || '';
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename={routerBaseName}>
     <App />
   </BrowserRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // The base path is set to the environment variable, or '/' for local development
-// const VITE_BASE_PATH = process.env.VITE_BASE_PATH || '/';
-const VITE_BASE_PATH = '/hds-training-vite';
+const VITE_BASE_PATH = process.env.VITE_BASE_PATH || '/';
 
 // https://vite.dev/config/
 export default defineConfig({
