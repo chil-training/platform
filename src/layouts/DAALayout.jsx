2ad3fe546f23fe0c30efa7a463e90ad39988813f
stmball <stmball@gmail.com>
2025-09-18 10:02:49 +0100

    Fix path?

--- src/layouts/DAALayout.jsx
+++ src/layouts/DAALayout.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Card from "../components/card";
+import Card from "../components/Card";
 import { Link } from "react-router";
 
 const DAALayout = ({ course, themes }) => {
