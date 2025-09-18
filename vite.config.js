b143dc680d624db9b030a8e4abba31cf548ffa7d
stmball <stmball@gmail.com>
2025-09-18 10:10:43 +0100

    Testing dynamic base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // The base path is set to the environment variable, or '/' for local development
-const VITE_BASE_PATH = process.env.VITE_BASE_PATH || '/';
+// const VITE_BASE_PATH = process.env.VITE_BASE_PATH || '/';
+const VITE_BASE_PATH = '/hds-training-vite/';
 
 // https://vite.dev/config/
 export default defineConfig({
