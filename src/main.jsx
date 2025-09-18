94f4f8143ffd9995d115da29fb0e28398caad33a
stmball <stmball@gmail.com>
2025-09-18 10:43:23 +0100

    Hashrouter?

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
 const routerBaseName = import.meta.env.VITE_BASE_PATH || '';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={routerBaseName}>
+  <HashRouter basename={routerBaseName}>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
