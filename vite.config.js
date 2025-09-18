bc191dd0b1a2013796e0eb5068553a1bf2ffb370
stmball <stmball@gmail.com>
2025-09-18 10:12:15 +0100

    Testing commit message

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // The base path is set to the environment variable, or '/' for local development
 // const VITE_BASE_PATH = process.env.VITE_BASE_PATH || '/';
-const VITE_BASE_PATH = '/hds-training-vite/';
+const VITE_BASE_PATH = '/hds-training-vite';
 
 // https://vite.dev/config/
 export default defineConfig({
